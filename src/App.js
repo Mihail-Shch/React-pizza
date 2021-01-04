d47247569601eb08cf53994fe28661bb526e7c3a
Mihail-Shch <alloha12345@mail.ru>
2021-01-05 00:06:17 +0300

    Added categories and Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Header } from "./components";
+import { Home, Cart } from "./pages";
+import axios from 'axios';
 
 function App() {
+
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas)
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path='/' render={() => <Home items={pizzas} />} exact />
+        <Route path='/cart' component={Cart} exact />
+      </div>
     </div>
   );
 }
